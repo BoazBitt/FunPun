0ff51fcec78ef11f11b3877fb066a9de326213aa
Boaz Bitton <boaz2119@gmail.com>
2023-06-17 13:43:15 +0300

    updated paths!

--- funpun/src/functions/path.js
+++ funpun/src/functions/path.js
@@ -1,2 +1,4 @@
-export const ClientPath = 'http://localhost:3000';
-export  const path = `http://127.0.0.1:8000`;
+// export const ClientPath = 'http://localhost:3000';
+export const ClientPath = 'https://fun-pun.onrender.com';
+// export  const path = `http://127.0.0.1:8000`;
+export  const path = `bittobo.pythonanywhere.com`;
