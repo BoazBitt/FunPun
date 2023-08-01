089714943362fd59d31444a64c89627547952d12
BoazBitt <73490112+BoazBitt@users.noreply.github.com>
2023-08-01 17:14:01 +0300

    Delete BackEnd directory

--- BackEnd/account/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Account
-from allSentences.models import Sentences
-
-# Register your models here.
-admin.site.register(Account)
-admin.site.register(Sentences)
--- BackEnd/allSentences/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Sentences
-
-
-class SentenceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sentences
-        fields = ['sentence', 'word', 'translation', 'Hword', 'level']
